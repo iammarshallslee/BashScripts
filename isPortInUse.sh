f43a5a336333f6def3375706e63abd0f96b37b32
Seyoung Lee <sylee@foodtechkorea.com>
2018-06-01 14:21:36 +0900

    added another condition

--- isPortInUse.sh
+++ isPortInUse.sh
@@ -2,9 +2,12 @@
 
 REGEX='^[0-9]+$'
 function isPortInUse() {
-	if ! [[ $1 =~ $REGEX ]]; then
-		echo "error: Not a number" >&2;
+	if [[ -z "$1" ]]; then
+		echo "Please specify a port, e.g. ./isPortInUse 80"
 		exit 1
+    elif ! [[ $1 =~ $REGEX ]]; then
+        echo "error: Not a number" >&2;
+        exit 1
 	else
 		if lsof -Pi :$1 -sTCP:LISTEN -t >/dev/null ; then
 			echo "Port $1 is in use."
