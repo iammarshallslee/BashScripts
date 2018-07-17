dd41a1eac0ffe558be851017022bf6cc6a4cb7a0
Seyoung Lee <sylee@foodtechkorea.com>
2018-07-17 15:39:33 +0900

    added a MySQL slave status checker

---
+++ mysql-checkslavestatus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### Parameters Guide ###
+# The first parameter ($1): mysql username
+# The second parameter ($2): password
+
+HOSTNAME=$(hostname)
+function checkSlaveStatus() {
+	SLAVE_IO_RUNNING=`mysql --login-path=check_slave_status -e 'show slave status\G' | grep Slave_IO_Running`
+	SLAVE_SQL_RUNNING=`mysql --login-path=check_slave_status -e 'show slave status\G' | grep Slave_SQL_Running`
+
+	if [[ $SLAVE_IO_RUNNING = *"Yes"* ]] && [[ $SLAVE_SQL_RUNNING = *"Yes"* ]]; then
+		echo "Both Slave IO and Slave SQL are running"
+	else
+		echo "There is a problem with the MySQL Slave."
+	fi
+}
+
+checkSlaveStatus
